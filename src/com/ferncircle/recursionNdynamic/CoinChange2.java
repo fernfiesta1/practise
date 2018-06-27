467e4a2e3d514caa590e6e57147f5e2e9d6003ed
fernfiesta1 <steffidbritto@gmail.com>
2018-06-27 11:23:31 -0700

    Testing

--- src/com/ferncircle/recursionNdynamic/CoinChange2.java
+++ src/com/ferncircle/recursionNdynamic/CoinChange2.java
@@ -4,8 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author dbrit00s
- * 
+ * @author dbrit00s * 
  * You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
 
 Note: You can assume that
